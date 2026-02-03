52c9519e253ef6741fa0ac7d369d5d196836a80e
zoroworld <pandamanishwork@gmail.com>
2026-02-03 23:09:36 +0530

    WIP: local changes

--- backend/app/ml/model_loader.py
+++ backend/app/ml/model_loader.py
@@ -3,7 +3,6 @@ import os
 from django.conf import settings
 
 MODEL_DIR = os.path.join(settings.BASE_DIR, "models")
-
 model = joblib.load(os.path.join(MODEL_DIR, "ensemble_model.pkl"))
 vectorizer = joblib.load(os.path.join(MODEL_DIR, "tfidf_vectorizer.pkl"))
 
