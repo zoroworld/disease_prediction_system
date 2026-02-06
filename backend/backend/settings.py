7bb6e0f99e42bb080011ab492445b5eb5c6e918e
zoroworld <pandamanishwork@gmail.com>
2026-02-06 15:11:19 +0530

    backend changes token

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # CORS FOR OUTER
-CORS_ALLOWED_ORIGINS = ['http://localhost:5173/']
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
 CORS_ALLOW_CREDENTIALS = True
 
 # whitenoise
